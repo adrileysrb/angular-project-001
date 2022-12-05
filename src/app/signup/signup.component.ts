c7b509ca0af7165b7b69f6eac40bb8fa31aa0370
Adriley Samuel Ribeiro Barbosa <asbarbosa@stefanini.com>
2022-12-05 15:11:34 -0300

    Create signup screen

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-
+  model = new User("", "");
 }
