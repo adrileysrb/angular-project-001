a64a545f0e921696ee39124c6e72c6989a70fd1d
Adriley Samuel Ribeiro Barbosa <asbarbosa@stefanini.com>
2022-12-05 14:57:23 -0300

    Create login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  model = new User("", "");
 }
